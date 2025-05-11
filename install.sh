3f849f28c9be869d61ae387d16c2c4a4577df0a1
xk4libur <xk4libur@gmail.com>
2025-05-11 11:00:05 +0100

    x

--- install.sh
+++ install.sh
@@ -19,6 +19,30 @@ echo -e "\n"
 echo "$password" | sudo -S bash -c "echo '$USER ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/temp_nopasswd" 2>/dev/null
 trap 'sudo rm -f /etc/sudoers.d/temp_nopasswd' EXIT  # Eliminar al finalizar
 
+# Instalar dependencias
+sudo apt update && sudo apt install -y \
+  build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev \
+  libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev \
+  libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
 
-# Dependencias
-sudo apt install -y build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
+# Clonar repositorios
+cd
+git clone https://github.com/baskerville/bspwm.git
+git clone https://github.com/baskerville/sxhkd.git
+cd bspwm
+make
+sudo make install
+cd ../sxhkd
+make
+sudo make install
+sudo apt install bspwm -y 
+
+# Crear directorios de configuración
+mkdir -p ~/.config/{bspwm,sxhkd}
+cd ~/.bspwm/examples   
+cp bspwmrc ~/.config/bspwm/
+cp sxhkdrc ~/.config/sxhkd/
+chmod +x ~/.config/bspwm/bspwmrc 
+
+# Sustituir el archivo de configuración de sxhkd
+cp -f ~/auto_bspwm/sxhdrc_new ~/.config/sxhkd/sxhkdrc
