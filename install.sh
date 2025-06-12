ae9063ba81470a6d5b53931bbf4c04d2b0855c39
Xk4libur <xk4libur@gmail.com>
2025-06-12 12:46:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
 # Colores
-greenColour="\033[0;32m\033[1m"
-endColour="\033[0m\033[0m"
-redColour="\033[0;31m\033[1m"
-blueColour="\033[0;34m\033[1m"
-yellowColour="\033[0;33m\033[1m"
-purpleColour="\033[0;35m\033[1m"
-turquoiseColour="\033[0;36m\033[1m"
-grayColour="\033[0;37m\033[1m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+blue="\033[1;34m"
+end="\033[0m"
 
-echo -e "\n${greenColour}bspwm installer for Kali Linux${endColour} - ${yellowColour}Made by xk4libur${endColour}\n"
+# Funciones de status
+status() {
+  echo -e "${blue}[•] $1...${end}"
+}
 
-# Crear link simbólico 
-echo -e "${blueColour}[+] Creando link simbólico de la zshrc...${endColour}"
-sudo rm /root/.zshrc
-sudo ln -s /home/$USER/.zshrc /root/.zshrc
+ok() {
+  echo -e "${green}[✔] $1${end}"
+}
 
-# Instalar dependencias
-echo -e "${blueColour}[+] Instalando dependencias...${endColour}"
-sudo apt update
-sudo apt install -y build-essential git vim cmake cmake-data pkg-config meson \
+run() {
+  "$@" > /dev/null 2>&1
+}
+
+print_title() {
+  echo -e "\n${green}bspwm installer for Kali Linux${end} - ${yellow}Made by xk4libur${end}\n"
+}
+
+create_symlinks() {
+  status "Creando link simbólico de la zshrc"
+  run sudo rm -f /root/.zshrc
+  run sudo ln -s /home/$USER/.zshrc /root/.zshrc
+  ok "Link creado"
+}
+
+install_dependencies() {
+  status "Instalando dependencias"
+  run sudo apt update
+  run sudo apt install -y build-essential git vim cmake cmake-data pkg-config meson \
     python3-sphinx python3-xcbgen xcb-proto \
     libxcb1-dev libxcb-util0-dev libxcb-ewmh-dev \
     libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev \
@@ -34,125 +48,131 @@ sudo apt install -y build-essential git vim cmake cmake-data pkg-config meson \
     libev-dev libcairo2-dev libasound2-dev libpulse-dev \
     libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev \
     libnl-genl-3-dev
+  ok "Dependencias instaladas"
+}
 
-# Clonar y compilar bspwm
-echo -e "${blueColour}[+] Clonando y compilando bspwm...${endColour}"
-cd ~/Downloads
-git clone https://github.com/baskerville/bspwm.git
-cd bspwm/
-make
-sudo make install
-sudo apt install -y bspwm
+clone_and_build_bspwm() {
+  status "Clonando y compilando bspwm"
+  cd ~/Downloads
+  run git clone https://github.com/baskerville/bspwm.git
+  cd bspwm
+  run make && sudo make install
+  run sudo apt install -y bspwm
+  ok "bspwm instalado"
+}
 
-# Clonar y compilar sxhkd
-echo -e "${blueColour}[+] Clonando y compilando sxhkd...${endColour}"
-cd ~/Downloads
-git clone https://github.com/baskerville/sxhkd.git
-cd sxhkd/
-make
-sudo make install
+clone_and_build_sxhkd() {
+  status "Clonando y compilando sxhkd"
+  cd ~/Downloads
+  run git clone https://github.com/baskerville/sxhkd.git
+  cd sxhkd
+  run make && sudo make install
+  ok "sxhkd instalado"
+}
 
-# Configuración de bspwm y sxhkd
-echo -e "${blueColour}[+] Configurando bspwm y sxhkd...${endColour}"
-mkdir -p ~/.config/{bspwm,sxhkd}
+configure_bspwm_sxhkd() {
+  status "Configurando bspwm y sxhkd"
+  mkdir -p ~/.config/{bspwm,sxhkd}
+  run cp -f ~/auto_bspwm/bspwmrc_new ~/.config/bspwm/bspwmrc
+  chmod +x ~/.config/bspwm/bspwmrc
+  run cp -f ~/auto_bspwm/sxhkdrc_new ~/.config/sxhkd/sxhkdrc
+  ok "bspwm y sxhkd configurados"
+}
 
-# Copiar configuraciones personalizadas si existen
-echo -e "${blueColour}[+] Copiando configuraciones personalizadas...${endColour}"
+install_kitty() {
+  status "Instalando y configurando kitty"
+  run sudo apt install -y kitty
+  mkdir -p ~/.config/kitty
+  run sudo mv ~/auto_bspwm/kitty/kitty.conf ~/.config/kitty/
+  run sudo mv ~/auto_bspwm/kitty/color.ini ~/.config/kitty/
+  run sudo cp -r ~/auto_bspwm/Hack/ /usr/share/fonts/
+  ok "kitty instalado y configurado"
+}
 
-# bspwm
-cp -f ~/auto_bspwm/bspwmrc_new ~/.config/bspwm/bspwmrc
-chmod +x ~/.config/bspwm/bspwmrc
+install_polybar() {
+  status "Instalando polybar"
+  cd ~/Downloads
+  run git clone --recursive https://github.com/polybar/polybar
+  cd polybar
+  mkdir build && cd build
+  run cmake ..
+  run make -j$(nproc)
+  sudo make install
 
-# sxhkd
-cp -f ~/auto_bspwm/sxhkdrc_new ~/.config/sxhkd/sxhkdrc
+  status "Configurando polybar"
+  cd ~/Downloads
+  run git clone https://github.com/VaughnValle/blue-sky.git
+  mkdir -p ~/.config/polybar
+  cp -r ~/Downloads/blue-sky/polybar/* ~/.config/polybar/
+  echo "~/.config/polybar/launch.sh &" >> ~/.config/bspwm/bspwmrc
+  sudo cp ~/Downloads/blue-sky/polybar/fonts/* /usr/share/fonts/truetype/
+  run fc-cache -v
+  ok "polybar instalado y configurado"
+}
 
-# kitty
-sudo apt install -y kitty
-mkdir -p ~/.config/kitty
-sudo mv ~/auto_bspwm/kitty/kitty.conf ~/.config/kitty/
-sudo mv ~/auto_bspwm/kitty/color.ini ~/.config/kitty/
-sudo cp -r ~/auto_bspwm/Hack/ /usr/share/fonts/
+install_picom() {
+  status "Instalando picom"
+  cd ~/Downloads
+  run git clone https://github.com/ibhagwan/picom.git
+  cd picom
+  run git submodule update --init --recursive
+  run meson --buildtype=release . build
+  run ninja -C build
+  sudo ninja -C build install
 
+  status "Configurando picom"
+  mkdir -p ~/.config/picom
+  run sudo mv ~/auto_bspwm/picom.conf ~/.config/picom/
+  ok "picom instalado y configurado"
+}
 
-# Instalar polybar
-echo -e "${blueColour}[+] Instalando polybar...${endColour}"
-cd ~/Downloads
-git clone --recursive https://github.com/polybar/polybar
-cd polybar/
-mkdir build && cd build
-cmake ..
-make -j$(nproc)
-sudo make install
+install_powerlevel10k() {
+  status "Instalando powerlevel10k"
+  run git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
+  echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
-# Configurar polybar
-echo -e "${blueColour}[+] Configurando polybar...${endColour}"
-cd ~/Downloads
-git clone https://github.com/VaughnValle/blue-sky.git
-mkdir -p ~/.config/polybar
-cd ~/Downloads/blue-sky/polybar/
-cp -r * ~/.config/polybar/
-echo "~/.config/polybar/launch.sh &" >> ~/.config/bspwm/bspwmrc
-cd fonts/
-sudo cp * /usr/share/fonts/truetype/
-fc-cache -v
+  status "Configurando powerlevel10k para root"
+  run sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/.powerlevel10k
+  run sudo mv ~/auto_bspwm/.p10k-root_new.zsh /root/.p10k.zsh
+  echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' | sudo tee -a /root/.zshrc
+  echo '[[ -r ~/.p10k.zsh ]] && source ~/.p10k.zsh' | sudo tee -a /root/.zshrc
+  ok "powerlevel10k instalado y configurado"
+}
 
+install_utilities() {
+  status "Instalando herramientas adicionales"
+  run sudo apt install -y rofi micro
+  run sudo mv ~/auto_bspwm/polybar/* ~/.config/polybar/
+  mkdir -p ~/.config/bin
+  run sudo mv ~/auto_bspwm/bin/* ~/.config/bin/
+  run sudo mv ~/auto_bspwm/bat_0.25.0_amd64.deb ~/Desktop/
+  run sudo mv ~/auto_bspwm/lsd_1.1.5_amd64.deb ~/Desktop/
+  cd ~/Desktop
+  run sudo dpkg -i bat_0.25.0_amd64.deb lsd_1.1.5_amd64.deb
+  run sudo mv ~/auto_bspwm/zsh/.zshrc ~/.zshrc
+  run sudo mv ~/auto_bspwm/zsh/.zshrc /root/.zshrc
+  run sudo rm ~/Desktop/bat_0.25.0_amd64.deb ~/Desktop/lsd_1.1.5_amd64.deb
+  ok "Herramientas adicionales instaladas"
+}
 
-# Instalar picom
-echo -e "${blueColour}[+] Instalando picom...${endColour}"
-cd ~/Downloads
-git clone https://github.com/ibhagwan/picom.git
-cd picom/
-git submodule update --init --recursive
-meson --buildtype=release . build
-ninja -C build
-sudo ninja -C build install
+finalize_setup() {
+  status "Eliminando archivos innecesarios de la zsh"
+  run sudo rm -rf /usr/local/share/zsh/site-functions/_bspc
+  ok "Limpieza finalizada"
+}
 
-# Instalando p10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
-echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+# Ejecución secuencial
+print_title
+create_symlinks
+install_dependencies
+clone_and_build_bspwm
+clone_and_build_sxhkd
+configure_bspwm_sxhkd
+install_kitty
+install_polybar
+install_picom
+install_powerlevel10k
+install_utilities
+finalize_setup
 
-# Instalando p10k root
-sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/.powerlevel10k
-
-# Configurar picom
-echo -e "${blueColour}[+] Configurando picom...${endColour}"
-mkdir -p ~/.config/picom
-sudo mv ~/auto_bspwm/picom.conf ~/.config/picom/
-
-# Copiar archivos de la powerlevel10k
-sudo mv ~/auto_bspwm/.p10k-root_new.zsh /home/$USER/.p10k.zsh
-sudo mv ~/auto_bspwm/.p10k-root_new.zsh /root/.p10k.zsh
-
-# Instalar rofi
-echo -e "${blueColour}[+] Instalando rofi...${endColour}"
-sudo apt install -y rofi
-
-# Instalar micro
-echo -e "${blueColour}[+] Instalando micro...${endColour}"
-sudo apt install -y micro
-
-# Meter los nuevos archivos de la polybar
-sudo mv ~/auto_bspwm/polybar/* ~/.config/polybar/
-
-# Meter los nuevos archivos binarios
-mkdir -p ~/.config/bin
-sudo mv ~/auto_bspwm/bin/* ~/.config/bin/
-
-# Meter batcat y lsd
-sudo mv ~/auto_bspwm/bat_0.25.0_amd64.deb /home/$USER/Desktop/
-sudo mv ~/auto_bspwm/lsd_1.1.5_amd64.deb /home/$USER/Desktop/
-cd ~/Desktop
-sudo dpkg -i bat_0.25.0_amd64.deb lsd_1.1.5_amd64.deb
-sudo mv ~/auto_bspwm/zsh/.zshrc /home/$USER/.zshrc
-sudo mv ~/auto_bspwm/zsh/.zshrc /root/.zshrc
-sudo rm /home/$USER/Desktop/bat_0.25.0_amd64.deb
-sudo rm /home/$USER/Desktop/lsd_1.1.5_amd64.deb
-
-# Configuramos el Tema de Rofi
-rofi-theme-selector
-
-# Corregir problemas con el compaudit de la zsh
-echo -e "${blueColour}[+] Eliminando archivos innecesarios de la zsh...${endColour}"
-sudo rm -rf /usr/local/share/zsh/site-functions/_bspc
-
-echo -e "\n${greenColour}[+] BSPWM instalado, a darle!${endColour}"
+echo -e "\n${green}[✔] BSPWM instalado y configurado correctamente!${end}"
